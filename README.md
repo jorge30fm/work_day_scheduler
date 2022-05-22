f3540acca72bd181a84d93c96a4f36c007b96926
Jorge Felix <jorge30fm@gmail.com>
2022-05-21 22:39:22 -0400

    finalized assignment

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # work_day_scheduler
+
+##Description
+
+This website serves as a scheduling app to keep track of daily tasks. Contains a dynamic layout, with javascript created element. Employsthird party API's like jQuery, Moment.js and Bootstrap.js to add functionality and simplify code. Each element in the page is dynamically created with java script. CSS classes and moment.js methods used to keep track of time and change textarea colors. Tasks saved to local storage so that user never loses the information unless updated. If the task is in the past, the text area turns gray, if it is in the present, it turns red, and if it is in the future, it turns green. Function that checks for the time in order to change styling set to run every 30 minutes.
+
+  <img src="./assets/images/127.0.0.1_5500_index.html.png">
--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,79 @@
 //get container that holdshour blocks
 var container = $(".container");
+var currentDate = moment().format('dddd' + ', ' + 'MMM Do YYYY');
+var currentDayEl = $('#currentDay');
+currentDayEl.text(currentDate);
 
-{/* <div class="input-group">
-        <span class="input-group-text">9:00 am</span>
-        <textarea class="form-control" aria-label="With textarea"></textarea>
-                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
-</div> */}
+
+
 var displayScheduler = function() {
-        for (i = 0; i < 8; i++) {
+        for (i = 9; i < 17; i++) {
                 var hourBlock = $("<div class='input-group'>");
-
-                var timeSlot = $("<span class='input-group-text col-1'>");
-
-                if (i < 3) {
-                        var timeDisplay = i + 9;
-                        timeSlot.text(timeDisplay + ' am');
-                } else if (i===3) {
-                        timeSlot.text('12 pm');
+                //create time list
+                var timeSlot = $("<span class='input-group-text hour col-2 col-md-2'>");
+                if (i < 12) {
+                        var timeDisplay = i;
+                        timeSlot.attr('data-time', i + ':00AM');
+                        timeSlot.text(timeDisplay + ':00 am');
+                } else if (i===12) {
+                        timeSlot.attr('data-time', '12:00PM')
+                        timeSlot.text(i +':00 pm');
                 } else {
-                        var timeDisplay = i - 3;
-                        timeSlot.text(timeDisplay + ' pm')
+                        var timeDisplay = i - 12;
+                        timeSlot.attr('data-time', timeDisplay + ':00PM');
+                        timeSlot.text(timeDisplay + ':00 pm')
                 }
                 hourBlock.append(timeSlot);
-                var textArea = $('<textarea class="form-control" aria-label="With textarea">');
+
+                //add text area
+                var textArea = $('<textarea class="form-control col-8 col-md-9" aria-label="With textarea">');
+                textArea.attr('id', 'textArea-' + (i));
                 hourBlock.append(textArea);
-                var btn = $('<button class="saveBtn col-2 btn-outline-secondary" type="button" id="button-addon2" style="--bs-bg-opacity: .5;">');
-                btn.attr('id', (i + 9));
+
+                //add button
+                var btn = $('<button class="saveBtn col-2 col-md-1 btn-outline-secondary" type="button" id="button-addon2" style="--bs-bg-opacity: .5;">');
                 var icon = $('<span class="oi oi-box">');
                 btn.append(icon);
                 hourBlock.append(btn);
                 container.append(hourBlock);
         }
 };
-displayScheduler()
+displayScheduler();
+$('.saveBtn').on('click', function(){
+        var buttonClicked = $(this)
+        var textElValue = buttonClicked.prev('textarea').val().trim();
+        var textEl = $(buttonClicked.prev('textarea'))
 
-/*0-9
-1-10
-2-11
-3-12
-4-1
-5-2
-6-3
-7-4
-*/
+        var textAreaID = textEl.attr('id');
+        localStorage.setItem(textAreaID, textElValue)
+})
+
+var loadText = function() {
+        for(i = 9; i < 17; i++){
+        var displayText = localStorage.getItem('textArea-'+ i);
+        $('#textArea-'+ i).  text(displayText);
+}
+}
+
+loadText();
+
+
+var pastPresentFuture = function () {
+        var time = moment().format('H')
+        for (i = 9; i < 17; i++ ){
+                if (i < time) {
+                        var content = $('#textArea-' + i);
+                        content.addClass("past");
+                }
+                else if (i == time) {
+                        var content = $('#textArea-' + i);
+                        content.addClass("present");
+                }
+                else {if (i > time) {
+                        var content = $('#textArea-' + i);
+                        content.addClass("future");
+                }
+        }
+        }
+}
+setInterval(pastPresentFuture(), (1000 * 60) * 30)
